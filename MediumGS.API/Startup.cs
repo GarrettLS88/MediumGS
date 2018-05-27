1d7afe4374733a091d9b19cc812606d6bf97712c
GarrettLS88 <GarrettLS88@protonmail.com>
2018-05-27 11:37:22 -0500

    Readded db context

--- MediumGS.API/Startup.cs
+++ MediumGS.API/Startup.cs
@@ -19,6 +19,11 @@ namespace MediumGS.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TestContext>(options =>
+                options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.RegisterServices();
+
             services.AddMvc();
         }
 
